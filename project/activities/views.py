d2e31ac60634aba81064c5df8dcf1bd5ddaa3fce
Oscar Roa <oscaroa196@gmail.com>
2021-08-20 13:53:28 -0500

    Add Activities viewsets and register in project's urls.

--- project/activities/views.py
+++ project/activities/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from activities.models import Activity
+from rest_framework import viewsets
+from activities.serializers import ActivitySerializer
 
-# Create your views here.
+
+class ActivityViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows  CRUD operations on the Activity model
+    """
+    queryset = Activity.objects.all()
+    serializer_class = ActivitySerializer
