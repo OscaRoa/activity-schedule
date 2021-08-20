ac15161157d1417c8bb831b665c9991ccb31b042
Oscar Roa <oscaroa196@gmail.com>
2021-08-20 13:52:29 -0500

    Add serializer for the model Activity.

---
+++ project/activities/serializers.py
@@ -0,0 +1,8 @@
+from activities.models import Activity
+from rest_framework import serializers
+
+
+class ActivitySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Activity
+        fields = ['property', 'schedule', 'title', 'created_at', 'updated_at', 'status']
