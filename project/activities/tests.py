1c9fa0af1ac8ae282e578f4d9dec0f7a4e6ef59c
Oscar Roa <oscaroa196@gmail.com>
2021-08-20 17:30:20 -0500

    Add dummy data for tests and add testcase docstring.

--- project/activities/tests.py
+++ project/activities/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+# from django.urls import reverse
+# from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class ActivityTests(APITestCase):
+    """Test case class to validate project's logic requirements.
+
+    Creation:
+        - Properties must be active.
+        - Schedule time must be available for a given property
+        - Activities last up to an hour
+
+    Update:
+        - Reschedule
+            - Only the schedule time can be changed, not the property.
+            - Canceled activities can't be rescheduled.
+        - Cancel
+            - Activity status is set to canceled.
+
+    List:
+        - Base filtering: now - 3 days <= schedule <= now + 2 weeks
+        - Survey is a hyperlinked key in the serializer
+        - Property is serialized inside an activity
+        - Condition:
+            - 'Pendiente a realizar': active status AND schedule >= now
+            - 'Atrasada': active status AND schedule <= now
+            - 'Finalizada': done status
+        - Fields included:
+            id, schedule, title, created_at, status and the ones above.
+    """
+    fixtures = ["data"]
+
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def test_dummy(self) -> None:
+        self.assertEqual(1, 1)
