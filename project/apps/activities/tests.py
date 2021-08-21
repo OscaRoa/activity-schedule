3837b9ec620059c6c07ff1ef13ff38599286ef9a
Oscar Roa <oscaroa196@gmail.com>
2021-08-21 13:56:51 -0500

    Update fixtures with the proper models and settings for testing.

--- project/apps/activities/tests.py
+++ project/apps/activities/tests.py
@@ -1,6 +1,10 @@
-# from django.urls import reverse
-# from rest_framework import status
+from datetime import datetime, timedelta
+from django.urls import reverse
+from rest_framework import status
 from rest_framework.test import APITestCase
+from apps.activities.models import Activity
+from apps.properties.models import Property
+from apps.activities.views import ActivityViewSet
 
 
 class ActivityTests(APITestCase):
@@ -29,10 +33,28 @@ class ActivityTests(APITestCase):
         - Fields included:
             id, schedule, title, created_at, status and the ones above.
     """
+
+    # 1 property enabled, 1 disabled and an scheduled activity on 02/09/21
     fixtures = ["data"]
 
-    def setUp(self) -> None:
-        return super().setUp()
+    def test_root(self) -> None:
+        """Test if the API root responds correctly.
+        """
+        url = reverse('api-root')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
 
-    def test_dummy(self) -> None:
-        self.assertEqual(1, 1)
+    def test_activity_creation_disabled_property(self):
+        """Test the creation of an activity with a disabled property.
+        """
+        url = reverse('activity-list')
+        data = {
+            "property": 2,  # Disabled property
+            "schedule": datetime.now() + timedelta(days=3),
+            "title": "An activity",
+            "status": "a"
+        }
+        response = self.client.post(url, data)
+        self.assertTrue(status.is_client_error(response.status_code))
+        self.assertEqual(Activity.objects.count(), 1)
